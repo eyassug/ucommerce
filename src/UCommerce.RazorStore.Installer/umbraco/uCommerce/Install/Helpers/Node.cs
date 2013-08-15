da942df971391570c95510e857af699b0b522a08
Tim Gaunt <tim@thesitedoctor.co.uk>
2013-08-15 20:12:50 +0100

    Slight optimisation to the active category filter

---
+++ src/UCommerce.RazorStore.Installer/umbraco/uCommerce/Install/Helpers/Node.cs
@@ -0,0 +1,19 @@
+﻿using umbraco.BusinessLogic;
+using umbraco.cms.businesslogic.web;
+
+namespace UCommerce.RazorStore.Installer.Helpers
+{
+    public class Node
+    {
+        public static void PublishChildDocs(Document doc)
+        {
+            doc.Publish(User.GetUser(0));
+
+            foreach (var childDoc in doc.Children)
+            {
+                PublishChildDocs(childDoc);
+            }
+        }
+    }
+
+}
