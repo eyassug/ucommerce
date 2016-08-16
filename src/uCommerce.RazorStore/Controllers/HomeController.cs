4b75adf05c7067756beb991bbdfede2b19fcca91
eaaalpur <eaaalpur@localhost>
2016-08-16 15:29:21 +0200

    Catalog restructured as Category View (not best practice)

---
+++ src/uCommerce.RazorStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+namespace UCommerce.MasterClass.Website.Controllers
+{
+	public class HomeController : RenderMvcController
+	{
+		public ActionResult Index()
+		{
+			return View("/views/frontpage.cshtml");
+		}
+	}
+}
